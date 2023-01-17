763eedd382947e645792b3a913007775a00373a4
Nick <nkfw3@yandex.ru>
2023-01-17 21:35:59 +0300

    Test code with the "Arcade" Module

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,16 @@
-Hi
+def cal_avg(a, b):
+    result = (a + b) / 2
+    return result
+x = 45
+y = 56
+print(cal_avg(x, y))
+# Example 8
+def a(x):
+    x = x + 1
+    return x
+
+
+x = 3
+x = a(x)
+
+print(x)
